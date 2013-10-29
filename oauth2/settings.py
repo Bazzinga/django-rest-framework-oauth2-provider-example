77551d4b79f6b9ed944731ea7620dbf61c0c3d48
erkarl <karl@karlranna.com>
2013-10-30 01:22:42 +0200

    Added django-cors-headers package.

--- oauth2/settings.py
+++ oauth2/settings.py
@@ -98,6 +98,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -120,6 +121,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
     'south',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
