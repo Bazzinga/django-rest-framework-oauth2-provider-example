501deb4612372825e152e1a44efceb1c6adefb2b
erkarl <karl@karlranna.com>
2013-10-30 01:36:42 +0200

    Enabled Django admin to test OAuth2.

--- oauth2/settings.py
+++ oauth2/settings.py
@@ -127,7 +127,7 @@ INSTALLED_APPS = (
     'provider.oauth2',
     'south',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
